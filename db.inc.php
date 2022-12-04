8415b4daffffe512973af78725f1972abc3c28cb
Adam N <96499144+Mizuukiiii@users.noreply.github.com>
2022-12-04 12:32:48 +0100

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,8 +1,6 @@
 <?php 
     $host = '127.0.0.1';
     $db = 'ip_3';
-    $user = 'www-aplikace';
-    $pass = 'Bezpe4n0Heslo.';
     $charset = 'utf8mb4';
     
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
